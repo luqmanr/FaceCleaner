b6e5e4107a80a17be51807fbec049caf4be2ddf0
luqman.rahardjo@gmail.com <luqman.rahardjo@gmail.com>
2020-05-13 13:17:30 +0700

    VER 1 DATACLEANING ARCFACE DONE

--- ArcFaceDataCleaning/dependencies/datacleaning_arcface.py
+++ ArcFaceDataCleaning/dependencies/datacleaning_arcface.py
@@ -12,7 +12,7 @@ ap.add_argument('-o', '--output',
                       if None specified, will be on "../output/')
 args = vars(ap.parse_args())
 
-input_path = '/Data/RKB-Dataset/Instagram_to_process/reference/14anra'
+input_path = '/Data/RKB-Dataset/Instagram_to_process/reference/cynthiahartonoo'
 output_path = '/Data/RKB-Dataset/Instagram_to_process/arcface_testing'
 
 def arrange_folder(input_path, output_path):
@@ -28,25 +28,29 @@ def arrange_folder(input_path, output_path):
         os.makedirs(dst_folder_raw)
 
     ## if image name contains the string 'train' it will be used as the reference image of that ID
-    for index, file in enumerate(files):
+    train_index = 0
+    raw_index = 0
+    for file in files:
         if "train" in str.lower(file):
             src = os.path.join(input_path, file)
-            dst = os.path.join(dst_folder_reference, file)
+            dst = os.path.join(dst_folder_reference, 'train_{}.jpg'.format(str(train_index)))
 
             ##copy 'train' files to new destination
             shutil.copy(src, dst)
+            train_index += 1
         else:
             src = os.path.join(input_path, file)
-            dst = os.path.join(dst_folder_raw, 'raw_{}.jpg'.format(str(index)))
+            dst = os.path.join(dst_folder_raw, 'raw_{}.jpg'.format(str(raw_index)))
 
             ##copy non 'train' files to new destination
             shutil.copy(src, dst)
+            raw_index += 1
 
 def generate_csv(input_path, output_path):
-    csv_mode = ['train', 'raw']
-    for mode in csv_mode:
+    csv_modes = ['train', 'raw']
+    for csv_mode in csv_modes:
         folder_ID = os.path.basename(input_path)
-        csv_folder = os.path.join(output_path, 'csv_{}'.format(mode))
+        csv_folder = os.path.join(output_path, 'csv_{}'.format(csv_mode))
         csv_output = os.path.join(csv_folder, (folder_ID + '.csv'))
 
         if not os.path.exists(csv_folder):
@@ -81,9 +85,9 @@ def reference_image_embeddings(input_path, output_path):
     csv_output = os.path.join(csv_folder, (folder_ID + '.csv'))
 
     for reference_image in reference_images:
+        img_name = os.path.basename(reference_image)
         try:
             embedding = core_arcface_cleaning.generate_embedding(reference_image)
-            img_name = os.path.basename(reference_image)
             embedding = np.append(embedding, img_name)
             write_embedding_to_csv(embedding, csv_output)
         except:
@@ -114,9 +118,9 @@ def raw_image_embeddings(input_path, output_path):
     csv_output = os.path.join(csv_folder, (folder_ID + '.csv'))
 
     for raw_image in raw_images:
+        img_name = os.path.basename(raw_image)
         try:
             embedding = core_arcface_cleaning.generate_embedding(raw_image)
-            img_name = os.path.basename(raw_image)
             embedding = np.append(embedding, img_name)
             write_embedding_to_csv(embedding, csv_output)
         except:
@@ -148,29 +152,100 @@ def read_csv_db(csv_path):
 
 def generate_distance(input_path, output_path):
     folder_ID = os.path.basename(input_path)
-    csv_folder = os.path.join(output_path, 'csv')
-    csv_path = os.path.join(csv_folder, (folder_ID + '.csv'))
 
-    raw_embeddings = raw_image_embeddings(input_path, output_path)
-    reference_embeddings, image_refs = read_csv_db(csv_path)
-    for index, raw_embedding in enumerate(raw_embeddings):
-        for reference_embedding in reference_embeddings:
+    ## locate the csv files
+    csv_train_folder = os.path.join(output_path, 'csv_train')
+    csv_train_path = os.path.join(csv_train_folder, (folder_ID + '.csv'))
+
+    csv_raw_folder = os.path.join(output_path, 'csv_raw')
+    csv_raw_path = os.path.join(csv_raw_folder, (folder_ID + '.csv'))
+
+    ## read the csv files
+    reference_embeddings, reference_img = read_csv_db(csv_train_path)
+    raw_embeddings, raw_img = read_csv_db(csv_raw_path)
+
+    similar_imgs = []
+
+    for index_raw, raw_embedding in enumerate(raw_embeddings):
+        print('processing', raw_img[index_raw], '===================')
+        # sim_list = []
+        for index_ref, reference_embedding in enumerate(reference_embeddings):
             # Compute squared distance between embeddings
-            dist = np.sum(np.square(raw_embedding-reference_embedding))
+            dist = np.sum(np.square(reference_embedding - raw_embedding))
             # Compute cosine similarity between embedddings
-            sim = np.dot(raw_embedding, reference_embedding.T)
-            # Print predictions
-            print('image_ref =', image_refs[index])
-            print('Distance = %f' %(dist))
-            print('Similarity = %f' %(sim))
+            sim = np.dot(reference_embedding, raw_embedding.T)
+
+            if sim > 0.5:
+                print('SIMILAR IMAGE', raw_img[index_raw])
+                # Print predictions
+                print('image_ref =', reference_img[index_ref])
+                print('image_raw =', raw_img[index_raw])
+                # print('Distance = %f' %(dist))
+                print('Similarity = %f' %(sim), '\n')
+                similar_imgs.append(raw_img[index_raw])
+            else:
+                print(raw_img[index_raw], 'not similar enough to', reference_img[index_ref])
+                print('Similarity = %f' %(sim), '\n')
+
+    similar_imgs = list(set(similar_imgs))
+    return similar_imgs
+
+def move_similar_to_train(input_path, output_path, similar_imgs):
+    folder_ID = os.path.basename(input_path)
+    folder_reference = os.path.join(output_path, 'reference', folder_ID)
+    folder_raw = os.path.join(output_path, 'raw', folder_ID)
+
+    folder_dst = os.path.join(output_path, 'reference', folder_ID)
+    # if not os.path.exists(folder_dst):
+    #     os.makedirs(folder_dst)
+
+    train_index = len(os.listdir(folder_reference))
+
+    for similar_img in similar_imgs:
+        src = os.path.join(folder_raw, similar_img)
+        dst = os.path.join(folder_dst, 'train_{}.jpg'.format(str(train_index)))
+
+        shutil.move(src, dst)
+        train_index += 1
+
+def write_new_csv(reference_csv, raw_csv, similar_imgs):
+    with open(raw_csv, newline='') as f:
+        reader = csv.reader(f)
+        embeddings_raw_csv = list(reader)
+
+    ## initialise raw embeddings that are going to be train embeddings
+    new_reference_embeddings = []
+    new_image_refs = []
+    ## the rest of embeddings that aren't going to be train embeddings are going to be written back to the raw csv
+    new_reference_embeddings = []
+    new_image_refs = []
+
+    for index, embedding in enumerate(embeddings_raw_csv):
+        if index == 0:
+            pass
+        else:
+            if embedding[-1] in similar_imgs:
+                new_image_refs.append(embedding[-1])
+                embedding.pop(0)
+                embedding.pop(-1)
+
+                for i in range(len(embedding)):
+                    embedding[i] = float(embedding[i])
+                
+                embedding = np.array(embedding)
+                new_reference_embeddings.append(embedding)
+
 
 print('======================================================')   
 print('TESTING GROUND') 
 # arrange_folder(input_path, output_path) 
-generate_csv(input_path, output_path)       
-# reference_image_embeddings(input_path, output_path)
-raw_image_embeddings(input_path, output_path)
-# generate_distance(input_path, output_path)
+for i in range(1):
+    generate_csv(input_path, output_path)       
+    reference_image_embeddings(input_path, output_path)
+    raw_image_embeddings(input_path, output_path)
+    similar_imgs = generate_distance(input_path, output_path)
+    move_similar_to_train(input_path, output_path, similar_imgs)
+
 
 '''
 image1 = './dataset/player1.jpg'
